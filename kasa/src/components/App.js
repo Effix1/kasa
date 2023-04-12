370f21887a481b6e51c17d3ed65537e2dc1a7514
Effix <107648239+Effix1@users.noreply.github.com>
2023-04-12 16:57:46 +0200

    initialisation kasa avec react

---
+++ kasa/src/components/App.js
@@ -0,0 +1,8 @@
+import '../App.css';
+import Banner from "./Banner"
+
+function App() {
+  return <Banner/>
+}
+
+export default App;
---
+++ kasa/src/components/Banner.js
@@ -0,0 +1,5 @@
+function Banner (){
+    return <h1>Kasa</h1>
+}
+
+export default Banner
