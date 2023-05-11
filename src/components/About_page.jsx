f7906937aeccbfc8dee3e0a028b0d2237a9dc291
Effix <107648239+Effix1@users.noreply.github.com>
2023-05-11 16:47:49 +0200

    ajout style about + animation card et chevron

--- src/components/About_page.jsx
+++ src/components/About_page.jsx
@@ -4,8 +4,9 @@ import chevron from '../assets/Vector.png';
 
 export default function AboutPage() {
   const cardParagraphRef = useRef(null);
+  const rotate = document.querySelector('about_button');
 
-  const handleButtonClick = () => {
+  const buttonOnClick = () => {
     const cardParagraph = cardParagraphRef.current;
 
     if (cardParagraph.style.display === 'block') {
@@ -13,17 +14,30 @@ export default function AboutPage() {
     } else {
       cardParagraph.style.display = 'block';
     }
+    if (cardParagraph.style.display === 'block') {
+      rotate.classList.toggle('rotate');
+    }
   };
 
   return (
     <div className='about_content'>
       <div className='about_card'>
-        <h2>Fiabilité</h2>
+        <div className='about_action_bar'>
+          <h2 className='about_tittle_card'>Fiabilité</h2>
+          <button onClick={buttonOnClick}>
+            <img src={chevron} alt='boutton' className='about_button' />
+          </button>
+        </div>
+        <p className='card_paragraph slide-down' ref={cardParagraphRef}>
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
       </div>
       <div className=' about_card card_respect'>
         <div className='about_action_bar'>
-          <h2>Respect</h2>
-          <button onClick={handleButtonClick}>
+          <h2 className='about_tittle_card'>Respect</h2>
+          <button onClick={buttonOnClick}>
             <img src={chevron} alt='boutton' className='about_button' />
           </button>
         </div>
@@ -35,13 +49,29 @@ export default function AboutPage() {
       </div>
       <div className='about_card'>
         <div className='about_action_bar'>
-          <h2>Service</h2>
-
-          <img src={chevron} alt='boutton' className='about_button' />
+          <h2 className='about_tittle_card'>Service</h2>
+          <button onClick={buttonOnClick}>
+            <img src={chevron} alt='boutton' className='about_button' />
+          </button>
         </div>
+        <p className='card_paragraph slide-down' ref={cardParagraphRef}>
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
       </div>
       <div className='about_card'>
-        <h2>Sécurité</h2>
+        <div className='about_action_bar'>
+          <h2 className='about_tittle_card'>Sécurité</h2>
+          <button onClick={buttonOnClick}>
+            <img src={chevron} alt='boutton' className='about_button' />
+          </button>
+        </div>
+        <p className='card_paragraph slide-down' ref={cardParagraphRef}>
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
       </div>
     </div>
   );
