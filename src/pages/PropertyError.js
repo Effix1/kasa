057f76847bc57c0f27c634691431b05f657ecf57
Effix <107648239+Effix1@users.noreply.github.com>
2023-05-31 17:15:35 +0200

    mise ne place et finition du responsive et version mobil de la page error

--- src/pages/PropertyError.js
+++ src/pages/PropertyError.js
@@ -2,11 +2,18 @@ import '../style/Error.css';
 import { NavLink } from 'react-router-dom';
 
 export default function Error() {
-  return <div className="div_error">
-    <h1 className="error_title">404</h1>
-    <p className="error_text">Oups! La page que vous demandez n'existe pas.</p>
-    <p className='error_link'><NavLink to='/' className={'style_link'}>Retourner sur la page d’accueil</NavLink></p>
-
-  </div>
-  ;
+  return (
+    <div className='div_error'>
+      <h1 className='error_title'>404</h1>
+      <p className='error_text'>
+        Oups! La page que <br className='error-mobil-text' />
+        vous demandez n'existe pas.
+      </p>
+      <p className='error_link'>
+        <NavLink to='/' className={'style_link'}>
+          Retourner sur la page d’accueil
+        </NavLink>
+      </p>
+    </div>
+  );
 }
