fae893925b68dca91ec329d2c2a7b621894d7ace
Effix <107648239+Effix1@users.noreply.github.com>
2023-06-04 00:18:55 +0200

    modification du style compnent cardslide

--- src/components/CardSlide.jsx
+++ src/components/CardSlide.jsx
@@ -2,19 +2,17 @@ import { useState } from 'react';
 import '../style/CardSlide.css';
 import chevron from '../assets/small_chevron.svg';
 
-export default function CardSlide({ text, className, children, margin }) {
+export default function CardSlide({ text, className, children }) {
   const [buttonOff, setButtonOff] = useState(false);
   const testClick = () => {
     setButtonOff(!buttonOff);
   };
 
-  const buttonOpen = `card_paragraph ${buttonOff ? 'open' : ''}`;
+  const buttonOpen = `card_paragraph ${buttonOff ? 'open' : ''} `;
   const rotateChevron = `card_chevron ${buttonOff ? 'rotate' : ''}`;
-  const style = {
-    marginBottom: { margin },
-  };
+
   return (
-    <div className={`card_content ${className}`} style={style}>
+    <div className={`card_content ${className}`}>
       <div className='card_header' onClick={testClick}>
         <p className='card_title'>{text}</p>
         <button>
